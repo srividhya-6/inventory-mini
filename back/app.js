64766130cb4079008d8d103751c4f05b14347229
srividhya-6 <137183398+srividhya-6@users.noreply.github.com>
2024-11-05 20:01:23 +0530

    Update app.js

--- back/app.js
+++ back/app.js
@@ -7,11 +7,13 @@ const orderRoute=require("./routes/orderRoute")
 const {route:userRoute,verifyToken}=require("./routes/userRoute")
 
 
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow only the frontend origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 app.use(userRoute);
 app.use(orderRoute);
