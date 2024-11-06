299f5342fc46ee902a0a64fc4e554a98cdb275dd
srividhya-6 <137183398+srividhya-6@users.noreply.github.com>
2024-11-06 21:22:13 +0530

    Update UserProductComponent.jsx

--- front/src/components/UserProductComponent.jsx
+++ front/src/components/UserProductComponent.jsx
@@ -36,13 +36,13 @@ export default function ProductComponent(){
     let [total,setTotal]=useState([]);
     let [search,setSearch]=useState("");
     useEffect(function updateProducts(){
-        axios.get("http://localhost:8082/api/products").then(response=> {
+        axios.get("https://inventory-mini.vercel.app/api/products").then(response=> {
             console.log(response.data);
             setProducts(response.data)
             setTotal(response.data)})
     },[])
     // function deleteProduct(id){
-    //     axios.delete(`http://localhost:8082/api/products/${id}`).then(res=>{
+    //     axios.delete(`https://inventory-mini.vercel.app/api/products/${id}`).then(res=>{
     //         console.log(res.data);
     //         setProducts(products.filter((p)=>p._id!=id))
     //     })
@@ -56,9 +56,9 @@ export default function ProductComponent(){
      }
     
      function addProduct(id){
-      axios.get(`http://localhost:8082/api/products/${id}`).then(res=>{
+      axios.get(`https://inventory-mini.vercel.app/api/products/${id}`).then(res=>{
         let product=res.data;
-        axios.get(`http://localhost:8082/api/orders/${uid}`).then(res=>{
+        axios.get(`https://inventory-mini.vercel.app/api/orders/${uid}`).then(res=>{
           let order=res.data;
           oid=order._id
           let r=order.items.find(p=>p.productId==id)
@@ -71,10 +71,10 @@ export default function ProductComponent(){
 
           order.totalPrice=order.totalPrice+product.price;
           order.status="place order"
-          axios.put(`http://localhost:8082/api/orders/${oid}`,order).then(res=>{
+          axios.put(`https://inventory-mini.vercel.app/api/orders/${oid}`,order).then(res=>{
             
             product.quantity=product.quantity-1
-            axios.put(`http://localhost:8082/api/products/${id}`,product).then(res=>{
+            axios.put(`https://inventory-mini.vercel.app/api/products/${id}`,product).then(res=>{
               setProducts(res.data)
               setTotal(res.data)
             }
@@ -229,13 +229,13 @@ export default function ProductComponent(){
 //     let [total,setTotal]=useState([]);
 //     let [search,setSearch]=useState("");
 //     useEffect(function updateProducts(){
-//         axios.get("http://localhost:8082/api/products").then(response=> {
+//         axios.get("https://inventory-mini.vercel.app/api/products").then(response=> {
 //             console.log(response.data);
 //             setProducts(response.data)
 //             setTotal(response.data)})
 //     },[])
 //     // function deleteProduct(id){
-//     //     axios.delete(http://localhost:8082/api/products/${id}).then(res=>{
+//     //     axios.delete(https://inventory-mini.vercel.app/api/products/${id}).then(res=>{
 //     //         console.log(res.data);
 //     //         setProducts(products.filter((p)=>p._id!=id))
 //     //     })
@@ -248,9 +248,9 @@ export default function ProductComponent(){
 //       setProducts(total.filter((p)=>p.name.toLowerCase().includes(search.toLowerCase())))
 //      }
 //     function addProduct(id){
-//       axios.get("http://localhost:8082/api/products/${id}").then(res=>{
+//       axios.get("https://inventory-mini.vercel.app/api/products/${id}").then(res=>{
 //         let product=res.data;
-//         axios.get("http://localhost:8082/api/orders/${uid}").then(res=>{
+//         axios.get("https://inventory-mini.vercel.app/api/orders/${uid}").then(res=>{
 //           let order=res.data;
 //           oid=order._id
 //           let r=order.items.find(p=>p.productId==id)
@@ -262,10 +262,10 @@ export default function ProductComponent(){
 //           }
 //           order.totalPrice=order.totalPrice+product.price;
 //           order.status="place order"
-//           axios.put("http://localhost:8082/api/orders/${oid}",order).then(res=>{
+//           axios.put("https://inventory-mini.vercel.app/api/orders/${oid}",order).then(res=>{
             
 //             product.quantity=product.quantity-1
-//             axios.put("http://localhost:8082/api/products/${id}",product).then(res=>{
+//             axios.put("https://inventory-mini.vercel.app/api/products/${id}",product).then(res=>{
 //               setProducts(res.data)
 //               setTotal(res.data)
 //             }
